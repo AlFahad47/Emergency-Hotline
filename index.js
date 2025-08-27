1725bd7d4f1649dd4b3ad6f7104d8c14c8ea98b6
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-08-28 01:21:10 +0600

    heart counter function added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("js file connected");
+let heartCounter = parseInt(document.getElementById("heart-counter").innerText);
+
+const heartClicks = document.getElementsByClassName("heart-icon");
+
+for (const heartClick of heartClicks) {
+  heartClick.addEventListener("click", function () {
+    heartCounter++;
+    document.getElementById("heart-counter").innerText = heartCounter;
+  });
+}
