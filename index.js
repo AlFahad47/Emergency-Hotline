29e6411a6d52aacdf03e605f854f8461bd411cb7
MD Fahad Molla <ahfahad47sss@gmail.com>
2025-08-28 15:15:22 +0600

    copy button features added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// heart counter
 let heartCounter = parseInt(document.getElementById("heart-counter").innerText);
 
 const heartClicks = document.getElementsByClassName("heart-icon");
@@ -6,5 +7,30 @@ for (const heartClick of heartClicks) {
   heartClick.addEventListener("click", function () {
     heartCounter++;
     document.getElementById("heart-counter").innerText = heartCounter;
+  });
+}
+
+// copy counter
+let copyCounter = parseInt(document.getElementById("copy-counter").innerText);
+
+const copyBtns = document.getElementsByClassName("copy-btn");
+
+for (const copyBtn of copyBtns) {
+  copyBtn.addEventListener("click", function () {
+    const emergencyCard = copyBtn.closest(".emergency-card");
+    // console.log(copyBtn, emergencyCard);
+    const number = emergencyCard.querySelector(".number").innerText;
+
+    navigator.clipboard
+      .writeText(number)
+      .then(() => {
+        alert("ফোন নাম্বার কপি হয়েছে : " + number);
+      })
+      .catch((err) => {
+        alert("ফোন নাম্বার কপি হয়নি !!!");
+      });
+
+    copyCounter++;
+    document.getElementById("copy-counter").innerText = copyCounter;
   });
 }
